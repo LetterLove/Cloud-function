fcd0ae5fae01422bcfab52de5f63db5b97993332
LetterLove <160673474+LetterLove@users.noreply.github.com>
2024-05-03 14:41:19 -0500

    Update login.js

--- api/login.js
+++ api/login.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 module.exports = async (req, res) => {
 
   // Set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', 'https://letterlove.github.io/Demostracion/');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
